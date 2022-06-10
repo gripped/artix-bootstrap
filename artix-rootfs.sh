3b2ca225b6561640160dd98ca0b19904bbde28a0
Chris Cromer <cromer@artixlinux.org>
2022-06-10 15:01:36 -0400

    remove suite66 and add date of tarball creation

--- artix-rootfs.sh
+++ artix-rootfs.sh
@@ -2,7 +2,7 @@
 
 set -e -u -o pipefail
 
-inits=('openrc' 'runit' 's6' 'suite66')
+inits=('openrc' 'runit' 's6' 'dinit')
 
 rm -rf --one-file-system download
 for init in "${inits[@]}"
@@ -10,7 +10,7 @@ do
 	./artix-bootstrap.sh -i $init -d download rootfs-$init
 	pushd rootfs-$init
 	rm var/cache/pacman/pkg/*
-	tar -cpzf ../rootfs.$init.tar.gz *
+	tar -cpzf ../artixlinux-bootstrap-$(date +'%Y.%m.%d')-$init-x86_64.tar.gz *
 	popd
 	rm -rf --one-file-system rootfs-$init
 done
